025fe1f0697b654fe536a6a79e0b1805cb313286
hiskio-daniel <daniel.yeh@hiskio.com>
2021-06-28 03:16:26 +0800

    update notification & user request

--- components/Global/DropDownMenuIcon.js
+++ components/Global/DropDownMenuIcon.js
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { apiPostReadNotifications} from '../../api/index'
+import { useDispatch } from 'react-redux';
+import { getMyInfo} from '../../redux/slices/userSlice'
 const DropDownMenuIcon = ({ Icon, children, title }) => {
+  const dispatch = useDispatch()
   const userInfo = useSelector((state) => state.user.userInfo);
   const [isMenuOpen, setMenuOpen] = useState(false);
+  const [unreadNotification ,setUnreadNotification] = useState(userInfo.unreadNotification)
+  const handleReadNotification = async() =>{
+    try{
+      setMenuOpen(!isMenuOpen)
+      if(title === 'Notification'){
+        setUnreadNotification(false)
+        const {data} =  await apiPostReadNotifications()
+        // Update userInfo
+        dispatch(getMyInfo())
+        console.log(data)
+      }
+    }catch(error){
+      console.log(error)
+    }
+  }
   return (
     <button className="group focus:outline-none">
-      {title === 'Notification' && userInfo.unreadNotification && (
+      {title === 'Notification' && unreadNotification && (
         <div className="relative">
-          <span className="z-50 absolute top-0 transform -translate-y-1/2 right-0 p-1 text-xs text-secondary rounded-full bg-red-400">
-            1
-          </span>
+          <p className="z-50 absolute text-white flex items-center justify-center top-0 transform -translate-y-1/2 right-0 w-[20px] h-[20px] text-xs rounded-full bg-red-400">
+            <span>1</span>
+          </p>
         </div>
       )}
       <Icon
-        onClick={() => setMenuOpen(!isMenuOpen)}
+        onClick={() => handleReadNotification()}
         className="icon group-hover:text-main group-focus:text-main"
       />{' '}
       
