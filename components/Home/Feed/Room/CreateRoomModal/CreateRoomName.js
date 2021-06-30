7b203535448c8a547013d04322b197f3f4a63b2b
hiskio-daniel <daniel.yeh@hiskio.com>
2021-06-30 13:04:00 +0800

    emoji create new name complete

--- components/Home/Feed/Room/CreateRoomModal/CreateRoomName.js
+++ components/Home/Feed/Room/CreateRoomModal/CreateRoomName.js
@@ -1,122 +1,131 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   UserIcon,
   ChevronLeftIcon,
   XIcon,
   PlusIcon
 } from '@heroicons/react/outline';
-import { stubFalse } from 'lodash';
-
-const roomNames = [
-  {
-    name: '小聚一下',
-    icon: '😅'
-  },
-  {
-    name: '一起來！',
-    icon: '😂'
-  },
-  {
-    name: '快來陪我',
-    icon: '🤣'
-  },
-  {
-    name: 'TGIF',
-    icon: '😊'
-  },
-  {
-    name: '畢業派對',
-    icon: '😇'
-  },
-  {
-    name: '在家好無聊',
-    icon: '🙂'
-  },
-  {
-    name: '歡樂時光',
-    icon: '😉'
-  },
-  {
-    name: '熱舞趴',
-    icon: '😉'
-  },
-  {
-    name: '家庭時光',
-    icon: '😉'
-  },
-  {
-    name: '午餐會',
-    icon: '😉'
-  },
-  {
-    name: '遊戲之夜',
-    icon: '😉'
-  },
-  {
-    name: '健身',
-    icon: '😉'
-  },
-  {
-    name: '追劇',
-    icon: '😉'
-  },
-  {
-    name: '團體聚會',
-    icon: '😉'
-  },
-  {
-    name: 'KTV',
-    icon: '😉'
-  },
-  {
-    name: '歡樂頌',
-    icon: '😉'
-  },
-  {
-    name: '生日',
-    icon: '🎂'
-  }
-];
-
+import { newIcons, roomNames } from '../../../../../utils/roomEmoji';
 const CreateRoomName = ({ setRoomInfo, roomInfo, setCreateNameOpen }) => {
+  const [newNameOpen, setNewNameOpen] = useState(false);
+  const [currentRoomInfo, setCurrentRoomInfo] = useState({
+    name: roomInfo.name,
+    icon: roomInfo.icon
+  });
   const handleSelectName = (room) => {
     setRoomInfo({ ...roomInfo, name: room.name, icon: room.icon });
     setCreateNameOpen(false);
   };
+  const handleChangeNewName = (value) => {
+    setCurrentRoomInfo({ ...currentRoomInfo, name: value });
+  };
+  const handleSelectNewIcon = (icon) => {
+    setCurrentRoomInfo({ ...currentRoomInfo, icon });
+  };
+  const handleSaveRoomInfo = () => {
+    setRoomInfo({
+      ...roomInfo,
+      name: currentRoomInfo.name,
+      icon: currentRoomInfo.icon
+    });
+    setCreateNameOpen(false);
+  };
   return (
     <div>
-      <div className="relative flex items-center text-secondary bg-secondary">
-        <span
-          onClick={() => setCreateNameOpen(false)}
-          className="cursor-pointer p-2 rounded-full bg-secondary shadow-lg text-secondary"
-        >
-          <ChevronLeftIcon className="h-6" />
-        </span>
-        <h2 className="text-lg sm:text-xl font-semibold absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2">
-          Room Name
-        </h2>
-      </div>
-      <hr className="my-4" />
-      <div className="max-h-[40vh] overflow-y-auto flex items-center whitespace-nowrap flex-shrink-0 flex-wrap">
-        <div className="flex  items-center min-w-[50%] p-3 cursor-pointer rounded-lg">
-          <span className="rounded-full p-2 mr-2 shadow-lg">
-            <PlusIcon className="h-6" />
-          </span>
-          <span>New Event</span>
-        </div>
-        {roomNames.map((room) => (
-          <div
-            key={room.name}
-            onClick={() => handleSelectName(room)}
-            className="min-w-[50%] p-3 cursor-pointer transition-all hover:brightness-110 hover:scale-120 rounded-lg"
-          >
-            <span className="rounded-full p-2 shadow-lg mr-2">
-              <span className="text-lg sm:text-2xl">{room.icon}</span>
+      {newNameOpen ? (
+        <div>
+          <div className="relative flex items-center text-secondary bg-secondary">
+            <span
+              onClick={() => setNewNameOpen(false)}
+              className="cursor-pointer p-2 rounded-full bg-secondary shadow-lg text-secondary"
+            >
+              <ChevronLeftIcon className="h-6" />
             </span>
-            <span>{room.name}</span>
+            <h2 className="text-lg sm:text-xl font-semibold absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2">
+              New Room Name
+            </h2>
           </div>
-        ))}
-      </div>
+          <hr className="my-4" />
+          <div>
+            <div className="border flex items-center rounded-lg w-full p-3">
+              <span className="bg-secondary mr-4 rounded-full w-[50px] h-[50px] flex items-center justify-center">
+                <span className="text-lg sm:text-2xl">
+                  {currentRoomInfo.icon}
+                </span>
+              </span>
+              <div>
+                <p className="text-xs text-main">Room Name</p>
+                <input
+                  className="focus:outline-none "
+                  value={currentRoomInfo.name}
+                  onChange={(e) => handleChangeNewName(e.target.value)}
+                  type="text"
+                  placeholder="New Name"
+                />
+              </div>
+            </div>
+            <div className="space-y-3 mt-4">
+              <h2 className="text-secondary text-lg sm:text-xl font-semibold">
+                Choose Emoji
+              </h2>
+              <div className="grid grid-cols-8">
+                {newIcons.map((icon) => (
+                  <div
+                    onClick={() => handleSelectNewIcon(icon)}
+                    className=" cursor-pointer p-3 rounded-full"
+                  >
+                    <span className="text-lg sm:text-2xl">{icon}</span>
+                  </div>
+                ))}
+              </div>
+              <button
+                onClick={() => handleSaveRoomInfo()}
+                className="w-full rounded-lg p-2 bg-main text-white hover:opacity-80"
+              >
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div className="relative flex items-center text-secondary bg-secondary">
+            <span
+              onClick={() => setCreateNameOpen(false)}
+              className="cursor-pointer p-2 rounded-full bg-secondary shadow-lg text-secondary"
+            >
+              <ChevronLeftIcon className="h-6" />
+            </span>
+            <h2 className="text-lg sm:text-xl font-semibold absolute left-1/2 transform -translate-x-1/2 top-1/2 -translate-y-1/2">
+              Room Name
+            </h2>
+          </div>
+          <hr className="my-4" />
+          <div className="max-h-[40vh] overflow-y-auto flex items-center whitespace-nowrap flex-shrink-0 flex-wrap">
+            <div
+              onClick={() => setNewNameOpen(true)}
+              className="flex  items-center min-w-[50%] p-3 cursor-pointer rounded-lg"
+            >
+              <span className="rounded-full p-2 mr-2 shadow-lg">
+                <PlusIcon className="h-6" />
+              </span>
+              <span>New Event</span>
+            </div>
+            {roomNames.map((room) => (
+              <div
+                key={room.name}
+                onClick={() => handleSelectName(room)}
+                className="min-w-[50%] p-3 cursor-pointer transition-all hover:brightness-110 hover:scale-120 rounded-lg"
+              >
+                <span className="rounded-full p-2 shadow-lg mr-2">
+                  <span className="text-lg sm:text-2xl">{room.icon}</span>
+                </span>
+                <span>{room.name}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
