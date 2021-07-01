0172b275882ae0c5a6e7821a89f09aaae6f76874
hiskio-daniel <daniel.yeh@hiskio.com>
2021-07-02 03:26:49 +0800

    added story

--- components/Home/Feed/Story/Stories.js
+++ components/Home/Feed/Story/Stories.js
@@ -3,34 +3,33 @@ import StoryCard from './StoryCard';
 import Image from 'next/image';
 import { PlusIcon } from '@heroicons/react/outline';
 import { useSelector } from 'react-redux';
-import genderAvatar from '../../../utils/genderAvatar';
+import genderAvatar from '../../../../utils/genderAvatar';
 const Stories = () => {
   const userInfo = useSelector((state) => state.user.userInfo);
 
   return (
-    <div className="flex w-full  space-x-1   overflow-hidden ">
-      <div className="relative h-44 flex-shrink-0   w-1/4 sm:w-1/5 flex flex-col rounded-md transition duration-75 cursor-pointer hover:opacity-80 ">
-        <div className="h-full w-full">
-          <Image
-            className=" h-full rounded-3xl"
-            src={userInfo.profileImage || genderAvatar(userInfo.gender)}
-            layout="fill"
-            objectFit="cover"
-          />
+    <div className="flex w-full mt-2 space-x-1   overflow-hidden ">
+      <div className="relative  flex-shrink-0   w-1/3 sm:w-1/4 xl:w-1/5  rounded-md transition duration-75 cursor-pointer hover:opacity-80 ">
+        <img
+          className="h-30 sm:h-56 w-full relative object-cover rounded-3xl rounded-b-none"
+          src={userInfo.profileImage || genderAvatar(userInfo.gender)}
+        />
+        <div className="relative text-sm sm:text-md px-1 py-5 rounded-b-3xl text-center bg-secondary text-secondary">
+          Create Story
+          <span className="rounded-full p-1 absolute z-10 -translate-y-1/2 top-0 text-white left-1/2 transform -translate-x-1/2 border-4 border-white bg-main ">
+            <PlusIcon className="h-6 " />
+          </span>
         </div>
-        <span className="rounded-full p-1 absolute z-10 top-1/2 text-secondary left-1/2 transform -translate-x-1/2 border-4 border-white bg-main ">
-          <PlusIcon className="h-6 " />
-        </span>
-        <div className="bg-secondary text-secondary h-1/2">Create Story</div>
       </div>
-      {stories.map((story) => (
+
+      {/* {stories.map((story) => (
         <StoryCard
           key={story.name}
           name={story.name}
           src={story.src}
           profile={story.profile}
         />
-      ))}
+      ))} */}
     </div>
   );
 };
