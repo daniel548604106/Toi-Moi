0d9d79cdd2eaf4877fc371368110b8682cb864bd
hiskio-daniel <daniel.yeh@hiskio.com>
2021-07-02 03:30:26 +0800

    added story

--- components/Home/Feed/Story/Stories.js
+++ components/Home/Feed/Story/Stories.js
@@ -9,9 +9,9 @@ const Stories = () => {
 
   return (
     <div className="flex w-full mt-2 space-x-1   overflow-hidden ">
-      <div className="relative  flex-shrink-0   w-1/3 sm:w-1/4 xl:w-1/5  rounded-md transition duration-75 cursor-pointer hover:opacity-80 ">
+      <div className="relative  flex-shrink-0   w-1/3 sm:w-1/4 2xl:w-1/5  rounded-md transition duration-75 cursor-pointer hover:opacity-80 ">
         <img
-          className="h-30 sm:h-56 w-full relative object-cover rounded-3xl rounded-b-none"
+          className="h-30 sm:h-44 w-full relative object-cover rounded-3xl rounded-b-none"
           src={userInfo.profileImage || genderAvatar(userInfo.gender)}
         />
         <div className="relative text-sm sm:text-md px-1 py-5 rounded-b-3xl text-center bg-secondary text-secondary">
