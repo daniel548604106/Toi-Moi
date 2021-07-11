9d8cfac92ee239c265502ccf52647674eef0a6a1
hiskio-daniel <daniel.yeh@hiskio.com>
2021-07-11 22:34:38 +0800

    fix saved posts

--- components/Saved/SavedCard.js
+++ components/Saved/SavedCard.js
@@ -26,36 +26,39 @@ const SavedCard = ({ post, publisher, type, handleRemoveSavedPost }) => {
   return (
     <div
       onClick={() => router.push(`/${publisher.username}/posts/${post._id}`)}
-      className="flex items-center justify-between p-2 rounded-lg shadow-lg cursor-pointer"
+      className="flex relative items-center w-full justify-between p-2 rounded-lg shadow-lg cursor-pointer"
     >
       <div className="flex items-center">
-        <Image
-          width={150}
-          height={150}
-          className="w-[60px] h-[60px] object-cover sm:w-[150px] sm:h-[150px] rounded-lg"
+        <img
+          className="w-[60px] h-[60px] sm:w-[100px] object-cover sm:h-[100px] rounded-lg"
           src={
             post.picUrl ||
             publisher.profileImage ||
             genderAvatar(publisher.gender)
           }
         />
-        <div className="ml-[10px]">
-          <p className="overflow-hidden truncate">{post.text}</p>
-          <p>{type}</p>
+        <div className="flex-1 ml-[10px] truncate space-y-2">
+          <p className="truncate overflow-hidden text-xs sm:text-sm w-[200px] sm:w-[300px]">
+            {post.text}
+          </p>
+          <span className="inline-block text-xs sm:text-sm p-1 sm:p-2 rounded-lg bg-main text-white">
+            {type}
+          </span>
         </div>
       </div>
       <span
         ref={elRef}
         onClick={(e) => handleToggleDropdown(e)}
-        className="relative rounded-full p-2 border"
+        className="top-1/2 right-2 transform -translate-y-1/2   absolute rounded-full p-2 border"
       >
-        <DotsHorizontalIcon className="h-6" />
+        <DotsHorizontalIcon className="w-4 h-4 sm:w-6" />
         {isDropdownShow && (
           <div
             onClick={() => handleDeletePost(post._id)}
-            className="absolute flex items-center transform rounded-lg translate-y-full right-0 bottom-0 p-2 border"
+            className="absolute flex items-center justify-center transform rounded-lg translate-y-full right-0 bottom-0 p-2 border"
           >
-            <TrashIcon className="h-6 mr-2" /> <span>Delete</span>
+            <TrashIcon className="h-4 sm:h-6 mr-2 " />{' '}
+            <span className="text-xs sm:text-sm">Delete</span>
           </div>
         )}
       </span>
